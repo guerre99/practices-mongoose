afb21254f262e677652ce1792a1984410ccdbaae
guerre99 <dgr761@gmail.com>
2023-11-03 13:03:28 +0100

    first commit

--- src/book.js
+++ src/book.js
@@ -1,19 +1,21 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
 const bookSchema = new mongoose.Schema({
-	title: String,
-	isbn: String,
-	status: String,
-	pageCount: String,
-	published: Boolean,
-	thumbnailUrl: String,
-	publishedDate: Date,
-	shortDescription: String,
-	longDescription: String,
-	authors: [{ type: String }],
-	categories: { type: String },
-});
+  title: { type: String, required: true },
+  isbn: { type: String, required: true, unique: true },
+  status: { type: String, required: true },
+  pageCount: Number,
+  published: { type: Boolean, default: true },
+  thumbnailUrl: String,
+  publishedDate: Date,
+  description: {
+    shortDescription: String,
+    longDescription: String,
+  },
+  authors: [{ type: String }],
+  categories: [{ type: String }],
+})
 
-const Book = mongoose.model('book', bookSchema);
+const Book = mongoose.model('book', bookSchema)
 
-module.exports = Book;
+module.exports = Book
